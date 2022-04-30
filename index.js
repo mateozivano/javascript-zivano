da75b14e306e355465525c6530b731d0070901e4
mateozivano <mattzivano@gmail.com>
2022-04-30 13:13:43 -0300

    desafio javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+let entrada = prompt("Adivine el número del 1 al 20");
+while(entrada != "6"){
+    alert("Número incorrecto, intente de nuevo");
+
+    entrada = prompt("Adivine el número del 1 al 20");
+}
