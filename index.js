6f54c7c705dcf29b9d94028257f32aaaf5d5f717
mateozivano <mattzivano@gmail.com>
2022-05-07 19:16:22 -0300

    creo simulador de creditos

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let entrada = prompt("Adivine el número del 1 al 20");
-while(entrada != "6"){
-    alert("Número incorrecto, intente de nuevo");
+let btn = document.getElementById("btn");
 
-    entrada = prompt("Adivine el número del 1 al 20");
-}
+btn.addEventListener("click", function(){
+    let cantidad = document.getElementById("cantidad").value;
+    let interes = document.getElementById("interes").value;
+   console.log(cantidad)
+   console.log(interes)
+    
+  let interesTotal =document.getElementById("interes_total").value = (cantidad / 100) * interes;
+  document.getElementById("costo_total").value = parseFloat(cantidad) + parseFloat(interesTotal);
+  console.log(interesTotal)
+ 
+})
