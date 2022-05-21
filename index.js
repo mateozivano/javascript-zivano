18ba979f03c5439443ccf28dd90aee5bbed5deb8
mateozivano <mattzivano@gmail.com>
2022-05-21 18:38:56 -0300

    agrego mensaje de bienvenida aleatorio

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ btn.addEventListener("click", function(){
    console.log(cantidad)
    console.log(interes)
     
-  let interesTotal =document.getElementById("interes_total").value = (cantidad / 100) * interes;
+  let interesTotal = document.getElementById("interes_total").value = (cantidad / 100) * interes;
   document.getElementById("costo_total").value = parseFloat(cantidad) + parseFloat(interesTotal);
   console.log(interesTotal)
  
 })
+
+
+//Saludo en html
+
+
+
+let saludosGenericos = ["Sea bienvenido a nuestra pagina" ,"Bienvenido a nuestro simulador de créditos", "Easycreditos te saluda" ];
+
+
+
+window.onload = function saludar() {
+  let saludo = document.getElementById("saludoGenerico");
+  saludo.innerHTML =
+      saludosGenericos[Math.floor(Math.random() * saludosGenericos.length)];
+}
+
+
+
 
