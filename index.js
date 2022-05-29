7cc3731267f33b5f091fa80c4d0471d21a16cbba
mateozivano <mattzivano@gmail.com>
2022-05-28 23:54:46 -0300

    cambios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let btn = document.getElementById("btn");
+let btn1 = document.getElementById("btn1");
 
-btn.addEventListener("click", function(){
+btn1.addEventListener("click", function(){
     let cantidad = document.getElementById("cantidad").value;
     let interes = document.getElementById("interes").value;
    console.log(cantidad)
@@ -9,17 +9,24 @@ btn.addEventListener("click", function(){
   let interesTotal = document.getElementById("interes_total").value = (cantidad / 100) * interes;
   document.getElementById("costo_total").value = parseFloat(cantidad) + parseFloat(interesTotal);
   console.log(interesTotal)
- 
+
+  //json
+    let var1 = {
+        "cantidad": document.getElementById("cantidad").value,
+        "interes": document.getElementById("interes").value,
+        "interesTotal": document.getElementById("interes_total").value,
+        "costoTotal": document.getElementById("costo_total").value};
+    console.log(var1)
+    let strVar1 = JSON.stringify(var1)
+    localStorage.setItem("calculo", strVar1);
+    
 })
 
 
 //Saludo en html
 
 
-
 let saludosGenericos = ["Sea bienvenido a nuestra pagina" ,"Bienvenido a nuestro simulador de créditos", "Easycreditos te saluda" ];
-
-
 
 window.onload = function saludar() {
   let saludo = document.getElementById("saludoGenerico");
@@ -27,6 +34,56 @@ window.onload = function saludar() {
       saludosGenericos[Math.floor(Math.random() * saludosGenericos.length)];
 }
 
+//aplicacion de session storage
 
+const MOON = '🌙';
+const SUN = '☀️';
+const DARK_MODE = 'dark';
+const LIGHT_MODE = 'light';
+const DEFAULT_MODE = DARK_MODE;
 
+const btn = document.querySelector('#theme-switcher');
+
+init();
+
+function init() {
+    let storedMode = sessionStorage.getItem('mode');
+    if (!storedMode) {
+        storedMode = DEFAULT_MODE;
+        sessionStorage.setItem('mode', DEFAULT_MODE);
+    }
+    setMode(storedMode);
+}
+
+function setMode(mode = DEFAULT_MODE) {
+    if (mode === DARK_MODE) {
+        btn.textContent = SUN;
+        document.body.classList.add(DARK_MODE);
+
+    } else if (mode === LIGHT_MODE) {
+        btn.textContent = MOON;
+        document.body.classList.remove(DARK_MODE);
+    }
+}
+
+btn.addEventListener('click', function () {
+    let mode = sessionStorage.getItem('mode');
+    if (mode) {
+        let newMode = mode == DARK_MODE ? LIGHT_MODE : DARK_MODE;
+        setMode(newMode);
+        sessionStorage.setItem('mode', newMode);
+    }
+});
+
+//json
+
+let json = localStorage.getItem("calculo")
+console.log(typeof json);
+console.log(json)
+let nro = (json.interesTotal);
+console.log(json.interesTotal);
+btn2.addEventListener("click", function() {
+    alert("cantidad deseada:", json.cantidad)
+    
+})
 
